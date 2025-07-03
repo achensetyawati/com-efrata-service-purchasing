def9a8ef80c86b93a92e5ecc653f7e8a88e3a49c
achensetyawati <achen.setyawati@gmail.com>
2025-07-03 16:17:16 +0700

    S14/25/add Id in stelling

--- Com.Efrata.Service.Purchasing.Lib/Facades/GarmentUnitReceiptNoteFacades/GarmentDOItemFacade.cs
+++ Com.Efrata.Service.Purchasing.Lib/Facades/GarmentUnitReceiptNoteFacades/GarmentDOItemFacade.cs
@@ -462,7 +462,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
                                //&& b.CreatedUtc >= QueryReceipt.Select(x=> x.UpdateDate).FirstOrDefault()
                                select new StellingViewModels
                                {
-
+                                   id = a.Id,
                                    POSerialNumber = null,
                                    Quantity = null,
                                    Uom = b.UomUnit,
@@ -524,6 +524,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
                 {
                     StellingEndViewModels stelling = new StellingEndViewModels
                     {
+                        id = a.id,
                         POSerialNumber = a.POSerialNumber,
                         Quantity = a.Quantity,
                         Uom = a.Uom,
